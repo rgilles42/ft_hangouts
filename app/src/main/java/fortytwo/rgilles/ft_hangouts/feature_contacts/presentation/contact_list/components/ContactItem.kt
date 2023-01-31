a80efaab2b56981cebcc4b7f6bf18a9a2675d64b
Raphaël Gilles <rgilles@student.42.fr>
2023-01-31 16:59:46 +0100

    Removed placeholder contact data from ContactItem composable

--- app/src/main/java/fortytwo/rgilles/ft_hangouts/feature_contacts/presentation/contact_list/components/ContactItem.kt
+++ app/src/main/java/fortytwo/rgilles/ft_hangouts/feature_contacts/presentation/contact_list/components/ContactItem.kt
@@ -1,6 +1,5 @@
 package fortytwo.rgilles.ft_hangouts.feature_contacts.presentation.contact_list.components
 
-import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -9,10 +8,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fortytwo.rgilles.ft_hangouts.feature_contacts.domain.model.Contact
-import java.time.LocalDate
 
 fun formContactName(contact: Contact): String {
     if (contact.firstName.isNotBlank() && contact.lastName.isNotBlank()) {
@@ -22,23 +19,23 @@ fun formContactName(contact: Contact): String {
 }
 
 @Composable
-@Preview(
-    showBackground = true,
-    //showSystemUi = true,
-    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL,
-    device = "id:pixel_5",
-    apiLevel = 33
-)
+//@Preview(
+//    showBackground = true,
+//    //showSystemUi = true,
+//    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL,
+//    device = "id:pixel_5",
+//    apiLevel = 33
+//)
 fun ContactItem(
     modifier: Modifier = Modifier,
-    contact: Contact = Contact(
-        null,
-        "Jean",
-        "Roulin",
-        "+33696969696",
-        "jean.roulin98015@gmail.com",
-        LocalDate.parse("2023-10-27")
-    )
+    contact: Contact
+//    = Contact(
+//        null,
+//        "Jean",
+//        "Roulin",
+//        "+33696969696",
+//        "jean.roulin98015@gmail.com"
+//    )
 ) {
     Box(
         modifier = modifier
