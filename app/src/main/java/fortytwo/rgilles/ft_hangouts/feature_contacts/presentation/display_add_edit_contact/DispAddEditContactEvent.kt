c6f0a4c97f9fb62f6e4f6cd3cbf22b9e8394c9b5
Raphaël GILLES <raphael.gilles@protonmail.com>
2023-02-22 17:41:44 +0100

    Contact now have images woopwoop

--- app/src/main/java/fortytwo/rgilles/ft_hangouts/feature_contacts/presentation/display_add_edit_contact/DispAddEditContactEvent.kt
+++ app/src/main/java/fortytwo/rgilles/ft_hangouts/feature_contacts/presentation/display_add_edit_contact/DispAddEditContactEvent.kt
@@ -1,10 +1,14 @@
 package fortytwo.rgilles.ft_hangouts.feature_contacts.presentation.display_add_edit_contact
 
+import android.content.Context
+import android.net.Uri
+
 sealed class DispAddEditContactEvent {
     data class EnteredFirstName(val value: String): DispAddEditContactEvent()
     data class EnteredLastName(val value: String): DispAddEditContactEvent()
     data class EnteredPhoneNumber(val value: String): DispAddEditContactEvent()
     data class EnteredEmail(val value: String): DispAddEditContactEvent()
+    data class ChangedPicture(val uri: Uri?, val context: Context): DispAddEditContactEvent()
     //data class ChangedBirthday(val date: LocalDate?): DispAddEditContactEvent()
     object SaveContact: DispAddEditContactEvent()
 }
