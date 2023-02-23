864b9d80e704a2326ff55db917b1a43d941569c2
Raphaël Gilles <rgilles@student.42.fr>
2023-02-23 19:50:03 +0100

    Changed field checks on contact add

--- app/src/main/java/fortytwo/rgilles/ft_hangouts/feature_contacts/domain/use_case/AddContactUseCase.kt
+++ app/src/main/java/fortytwo/rgilles/ft_hangouts/feature_contacts/domain/use_case/AddContactUseCase.kt
@@ -1,5 +1,6 @@
 package fortytwo.rgilles.ft_hangouts.feature_contacts.domain.use_case
 
+import android.util.Patterns
 import fortytwo.rgilles.ft_hangouts.feature_contacts.domain.model.Contact
 import fortytwo.rgilles.ft_hangouts.feature_contacts.domain.model.InvalidContactException
 import fortytwo.rgilles.ft_hangouts.feature_contacts.domain.repository.ContactRepository
@@ -12,8 +13,11 @@ class AddContactUseCase(
         if (contact.firstName.isBlank() and contact.lastName.isBlank()){
             throw InvalidContactException("Contact name cannot be empty.")
         }
-        if (contact.phoneNumber.isBlank() and contact.email.isBlank()){
-            throw InvalidContactException("Contact email or phone number cannot be empty.")
+        if (contact.phoneNumber.isNotBlank() && !Patterns.PHONE.matcher(contact.phoneNumber).matches()){
+            throw InvalidContactException("Contact phone number is invalid.")
+        }
+        if (contact.email.isNotBlank() && !Patterns.EMAIL_ADDRESS.matcher(contact.email).matches()){
+            throw InvalidContactException("Contact email is invalid.")
         }
         repository.insertContact(contact)
     }
