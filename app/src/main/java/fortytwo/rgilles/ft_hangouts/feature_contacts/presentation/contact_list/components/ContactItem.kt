b4190e0b3a529cab9be8becde0058e7853966162
Raphaël Gilles <rgilles@student.42.fr>
2023-02-23 11:29:46 +0100

    Made contact & conv list display contact picture if there is one

--- app/src/main/java/fortytwo/rgilles/ft_hangouts/feature_contacts/presentation/contact_list/components/ContactItem.kt
+++ app/src/main/java/fortytwo/rgilles/ft_hangouts/feature_contacts/presentation/contact_list/components/ContactItem.kt
@@ -1,5 +1,8 @@
 package fortytwo.rgilles.ft_hangouts.feature_contacts.presentation.contact_list.components
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -9,12 +12,18 @@ import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Message
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import fortytwo.rgilles.ft_hangouts.feature_contacts.domain.model.Contact
 import fortytwo.rgilles.ft_hangouts.feature_contacts.presentation.util.Screen
+import java.io.File
 
 fun formContactName(contact: Contact): String {
     if (contact.firstName.isNotBlank() && contact.lastName.isNotBlank()) {
@@ -29,6 +38,11 @@ fun ContactItem(
     contact: Contact,
     navController: NavController
 ) {
+    val context = LocalContext.current
+    val bitmap =  remember {
+        mutableStateOf<Bitmap?>(null)
+    }
+
     Box(
         modifier = modifier
             .fillMaxWidth()
@@ -45,15 +59,26 @@ fun ContactItem(
                 shape = CircleShape,
                 colors = CardDefaults.cardColors()
             ) {
-                //TODO: if contact has picture, else
-                Icon(
-                    modifier = modifier.fillMaxSize()
-                        .padding(5.dp),
-                    imageVector = Icons.Default.PersonOutline,
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.onSecondaryContainer
-                )
-
+                if (contact.picturePath.isNotBlank() && File(context.filesDir, contact.picturePath).exists()) {
+                    val source = ImageDecoder.createSource(File(context.filesDir, contact.picturePath))
+                    bitmap.value = ImageDecoder.decodeBitmap(source)
+                    bitmap.value?.let {  btm ->
+                        Image(
+                            bitmap = btm.asImageBitmap(),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+                } else {
+                    Icon(
+                        modifier = modifier.fillMaxSize()
+                            .padding(5.dp),
+                        imageVector = Icons.Default.PersonOutline,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSecondaryContainer
+                    )
+                }
             }
             Spacer(modifier = modifier.width(30.dp))
             Text(
--- app/src/main/java/fortytwo/rgilles/ft_hangouts/feature_messaging/presentation/conversation_list/components/ConversationItem.kt
+++ app/src/main/java/fortytwo/rgilles/ft_hangouts/feature_messaging/presentation/conversation_list/components/ConversationItem.kt
@@ -1,22 +1,36 @@
 package fortytwo.rgilles.ft_hangouts.feature_messaging.presentation.conversation_list.components
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PersonOutline
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import fortytwo.rgilles.ft_hangouts.common.domain.relations.ContactWithMessages
 import fortytwo.rgilles.ft_hangouts.feature_contacts.presentation.contact_list.components.formContactName
+import java.io.File
 
 @Composable
 fun ConversationItem(
     modifier: Modifier = Modifier,
     contactWithMessages: ContactWithMessages
 ){
+    val context = LocalContext.current
+    val bitmap =  remember {
+        mutableStateOf<Bitmap?>(null)
+    }
+
     Box(
         modifier = modifier
             .fillMaxWidth()
@@ -33,16 +47,26 @@ fun ConversationItem(
                 shape = CircleShape,
                 colors = CardDefaults.cardColors()
             ) {
-                //TODO: if contact has picture, else
-                Icon(
-                    modifier = modifier
-                        .fillMaxSize()
-                        .padding(5.dp),
-                    imageVector = Icons.Default.PersonOutline,
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.onSecondaryContainer
-                )
-
+                if (contactWithMessages.contact.picturePath.isNotBlank() && File(context.filesDir, contactWithMessages.contact.picturePath).exists()) {
+                    val source = ImageDecoder.createSource(File(context.filesDir, contactWithMessages.contact.picturePath))
+                    bitmap.value = ImageDecoder.decodeBitmap(source)
+                    bitmap.value?.let {  btm ->
+                        Image(
+                            bitmap = btm.asImageBitmap(),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+                } else {
+                    Icon(
+                        modifier = modifier.fillMaxSize()
+                            .padding(5.dp),
+                        imageVector = Icons.Default.PersonOutline,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSecondaryContainer
+                    )
+                }
             }
             Spacer(modifier = modifier.width(30.dp))
             Column {
