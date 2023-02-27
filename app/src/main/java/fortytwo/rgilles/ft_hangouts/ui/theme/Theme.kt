5656a8b9fdb4b206cfbf1af58e4dd309ed170cf5
Raphaël Gilles <rgilles@student.42.fr>
2023-02-27 14:26:31 +0100

    Started working on appbar color change feature

--- app/src/main/java/fortytwo/rgilles/ft_hangouts/ui/theme/Theme.kt
+++ app/src/main/java/fortytwo/rgilles/ft_hangouts/ui/theme/Theme.kt
@@ -3,13 +3,10 @@ package fortytwo.rgilles.ft_hangouts.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -36,6 +33,9 @@ private val LightColorScheme = lightColorScheme(
     onSurface = Color(0xFF1C1B1F),
     */
 )
+
+/*val ColorScheme.userPreferencePrimaryContainer: Color
+get() = if (true) Red300 else this@userPreferencePrimaryContainer.primaryContainer*/
 
 @Composable
 fun Ft_hangoutsTheme(
